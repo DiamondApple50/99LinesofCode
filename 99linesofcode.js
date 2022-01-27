5d7a3a691c2c2956a8faf082d05bc5c08571af98
jason loring <jasonaloring@gmail.com>
2022-01-27 14:18:12 -0600

    99LinesofCode lab

---
+++ 99linesofcode.js
@@ -0,0 +1,31 @@
+let friends = ['Will', 'Walker', 'Oxy', 'Cole', 'Tyler']
+
+
+
+
+function linessong() {
+
+}
+
+
+
+function loop(friends) {
+    for (let index = 100; index > 0; index--) {
+        if (index > 3) {
+            console.log(index - 1 + ' lines of code in the file. ', index - 1 + ' lines of code.', friends + ' strikes one out, clears it out', index - 2 + ' lines of code in the file');
+
+        } else if (index === 2) {
+            console.log(index  + ' lines of code in the file. ', index + ' lines of code.', friends + ' strikes one out, clears it out', index - 1 + ' line of code in the file');
+
+        } else if (index === 1) {
+            console.log(index + ' line of code in the file. ', index + ' line of code.', friends + ' strikes one out, clears it out.', index - 1 + ' lines of code in the file');
+        
+        } else if (index === 0) {
+            break
+        }
+    }
+}
+
+for (let index = 0; index < friends.length; index++) {
+    loop(friends[index])
+}
